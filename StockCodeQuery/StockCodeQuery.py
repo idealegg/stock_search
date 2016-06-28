9a541a1defcd438f40ec4fc4818da9fa1887e514
idealegg <452144941@qq.com>
2016-06-28 23:17:36 +0800

    because the module 'beatifulSoup' limits the length of the html file, so stock of shenzhen are lost.
    So use the regular expression directly to parse the html file.

--- StockCodeQuery/StockCodeQuery.py
+++ StockCodeQuery/StockCodeQuery.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import urllib2
 import re
+import pprint
+import chardet
 from bs4 import BeautifulSoup
 
 
@@ -18,11 +20,13 @@ class StockCodeQuery:
         req = urllib2.Request(url)
         resp = urllib2.urlopen(req)
         content = resp.read()
+        #pprint.pprint(content)
         return content
 
     def get_stock_code(self, content):
         soup = BeautifulSoup(content, 'lxml')
         items_list = soup.find_all(name='a', target='_blank')
+        #print soup.prettify()
         for item in items_list:
             match = re.search('(.*)\((\d+)\)', item.text)
             if match and item.has_attr('href'):
@@ -33,15 +37,29 @@ class StockCodeQuery:
                     code = match.group(0)
                     self.stock_map[name] = code
 
+    def get_stock_code2(self, content):
+        start = 0
+        match = re.search('<li><a target="_blank" href="http://quote.eastmoney.com/(..(\d+))\.html">(.*?)(\(\\2\))',
+                      content[start:])
+        while match:
+            name = match.group(3).decode('gbk')
+            code = match.group(1)
+            self.stock_map[name] = code
+            start += match.end()
+            match = re.search('<li><a target="_blank" href="http://quote.eastmoney.com/(..(\d+))\.html">(.*?)(\(\\2\))',
+                      content[start:])
+
     def update(self):
         content = self.get_html('shanghai')
-        self.get_stock_code(content)
-        content = self.get_html('shenzhen')
-        self.get_stock_code(content)
+        self.get_stock_code2(content)
+        #content = self.get_html('shenzhen')
+        #self.get_stock_code(content)
 
     def pprint(self):
         #pprint.pprint(self.stock_map)
         for key in self.stock_map:
+            # encoding = chardet.detect(key)['encoding']
+            # tmp_key =
             print "%20s\t %10s\n" % (key, self.stock_map[key])
 
     @classmethod
