5bdbdc7a245537fe12e5e50b242e6f6cfc3450bb
huangd <452144941@qq.com>
2019-07-16 10:53:45 +0800

    1. separate trigger list and stock list.

--- Monitor_Stock.py
+++ Monitor_Stock.py
@@ -14,27 +14,38 @@ import threading
 class MointorStock:
   def __init__(self, conf_path):
     self.TRIGGER_BUY_1_MONITOR = 'monitor_buy_1'
+    self.JUST_PRINT = 'just_print'
     self.config_parser = ConfigParser.ConfigParser()
     self.conf_path = conf_path
     self.work_time = (('0930', '1130'), ('1300', '1457')
                       #, ('0000', '2359')
                       )
-    self.conf = {'interval' : 10,
-                 'report_interval' : 1800,
-                 'monitor_buy_1': {
-                                    'min_trade_vol' : 5000,
-                                    'min_trade_rate': 0.1
-                  }
+    self.conf = {'interval': 10,
+                 'retry_interval': 60,
+                 'report_interval': 1800,
+                 'line_length': 240,
+                 self.TRIGGER_BUY_1_MONITOR: {
+                                    'min_trade_vol': 5000.0,
+                                    'min_trade_rate': 0.1,
+                                    'min_buy_1_vol': 10000.0,
+                                    'stock' : []
+                  },
+                 self.JUST_PRINT : {
+                                    'stock' : []
+
+                 }
                  }
     self.base_url = 'http://qt.gtimg.cn/q=%s'
-    self.stock_list = []
-    self.trigger = {self.TRIGGER_BUY_1_MONITOR: self.monitor_buy_1}
+    #self.stock_list = []
+    self.trigger = {self.TRIGGER_BUY_1_MONITOR: self.monitor_buy_1,
+                    self.JUST_PRINT : self.get_new_msg}
     self.trigger_list = []
     self.last_msg = {}
     self.cur_msg = {}
     self.warning_list = []
     self.warning_msg = ''
     self.cur_trigger = None
+    self.print_out_list = (1, 3, 4, 5, 6, 9, 10, 32, 33, 34, 38, 43)
     self.Field_content = {0: '未知',
                  1: '名字',
                  2: '代码',
@@ -92,10 +103,16 @@ class MointorStock:
     self.config_parser.read(self.conf_path)
     self.conf.update(dict(self.config_parser.items('GLOBAL')))
     self.trigger_list = re.split(re.compile('[,\s]+'), self.conf['trigger'])
-    self.stock_list = re.split(re.compile('[,\s]+'), self.conf['stock'])
+    #self.stock_list = re.split(re.compile('[,\s]+'), self.conf['stock'])
+    if self.config_parser.has_section(self.TRIGGER_BUY_1_MONITOR):
+      self.conf[self.TRIGGER_BUY_1_MONITOR] = dict(self.config_parser.items(self.TRIGGER_BUY_1_MONITOR))
+      self.conf[self.TRIGGER_BUY_1_MONITOR]['stock'] = re.split(re.compile('[,\s]+'), self.conf[self.TRIGGER_BUY_1_MONITOR]['stock'])
+    if self.config_parser.has_section(self.JUST_PRINT):
+      self.conf[self.JUST_PRINT] = dict(self.config_parser.items(self.JUST_PRINT))
+      self.conf[self.JUST_PRINT]['stock'] = re.split(re.compile('[,\s]+'), self.conf[self.JUST_PRINT]['stock'])
 
   def warning(self):
-    print self.warning_msg
+    print self.warning_msg.decode('gdb')
     self.warning_list.append("%s: %s: %s" % (time.ctime(), self.cur_trigger, self.warning_msg))
     t1 = threading.Thread(target=win32api.MessageBox, args=(0, self.warning_msg,
                 "%s %s" % (self.cur_trigger, time.ctime()),
@@ -106,47 +123,66 @@ class MointorStock:
     #                       win32con.MB_OK | win32con.MB_ICONWARNING)
 
   def get_new_msg(self, stock):
-    req = requests.get(self.base_url % stock)
+    while True:
+      try:
+        req = requests.get(self.base_url % stock)
+      except Exception, e:
+        print "Exception in get_new_msg:"
+        print e.message
+        time.sleep(float(self.conf['retry_interval']))
+        continue
+      break
     print req
-    print req.encoding
+    #print req.encoding
     #print req.content
     if self.cur_msg.has_key(stock):
       self.last_msg[stock] = self.cur_msg[stock]
     self.cur_msg[stock] = []
     self.cur_msg[stock] = req.content.split('~')
     encoding = req.encoding
+    self.cur_msg[stock][1] = self.cur_msg[stock][1].decode(encoding).encode('utf8')
     req.close()
     #pprint.pprint(self.cur_msg[stock])
-    return self.cur_msg[stock][1].decode(encoding).encode('utf8')
+    self.print_stock_info(stock)
+    #return self.cur_msg[stock][1]
+
+  def print_stock_info(self, stock):
+    s = ''
+    if self.cur_msg.has_key(stock):
+      for field in self.print_out_list:
+        s = ', '.join([s, "%s: %s" % (self.Field_content[field], self.cur_msg[stock][field])])
+        if len(s) > int(self.conf['line_length']):
+          print s
+          s = ''
+      print s
 
   def monitor_buy_1(self, stock):
-    print self.get_new_msg(stock)
-    self.TRIGGER_BUY_1_MONITOR = 'monitor_buy_1'
-    if self.config_parser.has_section(self.TRIGGER_BUY_1_MONITOR):
-      self.conf[self.TRIGGER_BUY_1_MONITOR] = dict(self.config_parser.items(self.TRIGGER_BUY_1_MONITOR))
+    self.get_new_msg(stock)
+    #self.TRIGGER_BUY_1_MONITOR = 'monitor_buy_1'
     if self.last_msg.has_key(stock):
-      if self.last_msg[stock][19] != self.cur_msg[stock][19]:
+      if self.last_msg[stock][9] != self.cur_msg[stock][9]:
         self.cur_trigger = self.TRIGGER_BUY_1_MONITOR
         self.warning_msg = "%s buy_1 price decreased from %s to %s!" % (
-                     self.cur_msg[stock][1], self.last_msg[stock][19], self.cur_msg[stock][19])
+                     self.cur_msg[stock][1].decode('utf8').encode('gbk'),
+                     self.last_msg[stock][9], self.cur_msg[stock][9])
         self.warning()
         return
-      last = float(self.last_msg[stock][19]) # buy_1 price
-      cur = float(self.cur_msg[stock][19])
-      if last > cur:
-        last = self.last_msg[stock][20]   # buy_1 trade volume
-        cur = self.cur_msg[stock][20]
-        if last - cur > self.conf[self.TRIGGER_BUY_1_MONITOR]['min_trade_vol'] \
-          and (last - cur)/last > self.conf[self.TRIGGER_BUY_1_MONITOR]['min_trade_rate']:
-          self.cur_trigger = self.TRIGGER_BUY_1_MONITOR
-          self.warning_msg = "%s buy_1 trade vol decreased from %s to %s!" % (
-            self.cur_msg[stock][1], last, cur)
-          self.warning()
+      #self.print_stock_info(stock)
+      last = float(self.last_msg[stock][10])   # buy_1 trade volume
+      cur = float(self.cur_msg[stock][10])
+      #pprint.pprint(self.conf)
+      if last - cur > float(self.conf[self.TRIGGER_BUY_1_MONITOR]['min_trade_vol']) \
+        or (last - cur)/last > float(self.conf[self.TRIGGER_BUY_1_MONITOR]['min_trade_rate']) \
+        or cur < float(self.conf[self.TRIGGER_BUY_1_MONITOR]['min_buy_1_vol']):
+        self.cur_trigger = self.TRIGGER_BUY_1_MONITOR
+        self.warning_msg = "%s buy_1 trade vol decreased from %s to %s!" % (
+          self.cur_msg[stock][1].decode('utf8').encode('gbk'), last, cur)
+        self.warning()
 
   def is_in_working_time(self, t=None):
     t1 = time.localtime(t)
     d = time.strftime("%y%m%d", t1)
-    print "\n--- check time: %s" % time.strftime("%y-%m-%d %H:%M:%S", t1)
+    #print "\n--- check time: %s" % time.strftime("%y-%m-%d %H:%M:%S", t1)
     for seg in self.work_time:
       start_time = time.strptime(''.join([d,seg[0]]), '%y%m%d%H%M')
       end_time = time.strptime(''.join([d,seg[1]]), '%y%m%d%H%M')
@@ -162,8 +198,8 @@ if __name__ == "__main__":
   while True:
     if ms.is_in_working_time():
       for t in ms.trigger_list:
-        for stock in ms.stock_list:
-          print "Trigger [%s]: [%s]" % (t, stock)
+        for stock in ms.conf[t]['stock']:
+          print "\n%s | Trigger [%s]: [%s]" % (time.ctime(), t, stock)
           ms.trigger[t](stock)
     else:
       print "current is not working time!"
